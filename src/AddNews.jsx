0986ff2a307f0da1e26993590113dd3415216f87
Ali <alimadadi110@gmail.com>
2025-02-25 16:06:10 +0330

    Gold price bar restyled and addnews translated

--- src/AddNews.jsx
+++ src/AddNews.jsx
@@ -143,7 +143,7 @@ export default function AdminPanel() {
                   }`}
                   onClick={() => setActiveTab("news")}
                 >
-                  اخبار
+                  خبرها
                 </li>
                 <li
                   className={`p-2 cursor-pointer ${
