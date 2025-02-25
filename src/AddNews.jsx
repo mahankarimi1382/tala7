401724400467ead8d1e4d757001616d5a2de3f8a
AliMadadi <alimadadi110@gmail.com>
2025-02-25 12:11:46 -0800

    KHABARHAYE KHOOB

--- src/AddNews.jsx
+++ src/AddNews.jsx
@@ -143,7 +143,7 @@ export default function AdminPanel() {
                   }`}
                   onClick={() => setActiveTab("news")}
                 >
-                  خبرها
+                  خبرهای خوب
                 </li>
                 <li
                   className={`p-2 cursor-pointer ${
