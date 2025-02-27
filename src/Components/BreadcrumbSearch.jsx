dd07506b804ea9406cde7791ba631c5e8c16f70c
AliMadadi <alimadadi110@gmail.com>
2025-02-27 04:22:48 -0800

    div  under breadcrumb created

--- src/Components/BreadcrumbSearch.jsx
+++ src/Components/BreadcrumbSearch.jsx
@@ -1,24 +1,195 @@
 import { Search } from "lucide-react";
+import PishnahadCards from "../PishnahadCards";
+import img1 from "../assets/img/containerring1.png";
+import img2 from "../assets/img/containerring2.png";
 
 export default function BreadcrumbSearch() {
   return (
-    <div className=" mt-6 md:mt-1 md:p-4 space-y-4 ms-10 sm:ms-14 ">
-      {/* Breadcrumb */}
-      <nav className="text-gray-500 text-sm">
-        <a href="#" className="hover:text-black">خانه</a>
-        <span className="mx-1">/</span>
-        <a href="#" className="text-black font-bold">دسته بندی نشده</a>
-      </nav>
+    <div className="flex flex-col">
+      <div className=" mt-6 md:mt-1 md:p-4 space-y-4 ms-10 sm:ms-14 ">
+        {/* Breadcrumb */}
+        <nav className="text-gray-500 text-sm">
+          <a href="#" className="hover:text-black">
+            خانه
+          </a>
+          <span className="mx-1">/</span>
+          <a href="#" className="text-black font-bold">
+            دسته بندی نشده
+          </a>
+        </nav>
 
-      {/* Search Bar */}
-      <div className="relative md:w-[60vw] w-[80vw] max-w-4xl ">
-        <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={18} />
-        <input
-          type="text"
-          placeholder="جستجوی محصولات"
-          className="w-full border border-gray-300 rounded-md py-2 pl-10 pr-4 text-right placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
-        />
+        {/* Search Bar */}
+        <div className="relative md:w-[60vw] w-[80vw] max-w-4xl ">
+          <Search
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+            size={18}
+          />
+          <input
+            type="text"
+            placeholder="جستجوی محصولات"
+            className="w-full border border-gray-300 rounded-md py-2 pl-10 pr-4 text-right placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
+          />
+        </div>
       </div>
-    </div>
+      <div className="grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 CardsArea mx-auto">
+
+        <PishnahadCards
+          ImageAddress={img1}
+          SecondImageAddress={img2}
+          RingName="حلقه زیبا"
+          FirstPrice="500000"
+          SecondPrice="900000"
+          MySecond="57"
+          MyMiniute="35"
+          MyHour="11"
+          MyDays="143"
+          MyDiscount="17"
+          MinusOrPlus="-"
+        />
+        <PishnahadCards
+          ImageAddress={img1}
+          SecondImageAddress={img2}
+          RingName="حلقه زیبا"
+          FirstPrice="500000"
+          SecondPrice="900000"
+          MySecond="57"
+          MyMiniute="35"
+          MyHour="11"
+          MyDays="143"
+          MyDiscount="17"
+          MinusOrPlus="-"
+        />{" "}
+        <PishnahadCards
+          ImageAddress={img1}
+          SecondImageAddress={img2}
+          RingName="حلقه زیبا"
+          FirstPrice="500000"
+          SecondPrice="900000"
+          MySecond="57"
+          MyMiniute="35"
+          MyHour="11"
+          MyDays="143"
+          MyDiscount="17"
+          MinusOrPlus="-"
+        />{" "}
+        <PishnahadCards
+          ImageAddress={img1}
+          SecondImageAddress={img2}
+          RingName="حلقه زیبا"
+          FirstPrice="500000"
+          SecondPrice="900000"
+          MySecond="57"
+          MyMiniute="35"
+          MyHour="11"
+          MyDays="143"
+          MyDiscount="17"
+          MinusOrPlus="-"
+        />{" "}
+        <PishnahadCards
+          ImageAddress={img1}
+          SecondImageAddress={img2}
+          RingName="حلقه زیبا"
+          FirstPrice="500000"
+          SecondPrice="900000"
+          MySecond="57"
+          MyMiniute="35"
+          MyHour="11"
+          MyDays="143"
+          MyDiscount="17"
+          MinusOrPlus="-"
+        />{" "}
+        <PishnahadCards
+          ImageAddress={img1}
+          SecondImageAddress={img2}
+          RingName="حلقه زیبا"
+          FirstPrice="500000"
+          SecondPrice="900000"
+          MySecond="57"
+          MyMiniute="35"
+          MyHour="11"
+          MyDays="143"
+          MyDiscount="17"
+          MinusOrPlus="-"
+        />{" "}
+        <PishnahadCards
+          ImageAddress={img1}
+          SecondImageAddress={img2}
+          RingName="حلقه زیبا"
+          FirstPrice="500000"
+          SecondPrice="900000"
+          MySecond="57"
+          MyMiniute="35"
+          MyHour="11"
+          MyDays="143"
+          MyDiscount="17"
+          MinusOrPlus="-"
+        />{" "}
+        <PishnahadCards
+          ImageAddress={img1}
+          SecondImageAddress={img2}
+          RingName="حلقه زیبا"
+          FirstPrice="500000"
+          SecondPrice="900000"
+          MySecond="57"
+          MyMiniute="35"
+          MyHour="11"
+          MyDays="143"
+          MyDiscount="17"
+          MinusOrPlus="-"
+        />{" "}
+        <PishnahadCards
+          ImageAddress={img1}
+          SecondImageAddress={img2}
+          RingName="حلقه زیبا"
+          FirstPrice="500000"
+          SecondPrice="900000"
+          MySecond="57"
+          MyMiniute="35"
+          MyHour="11"
+          MyDays="143"
+          MyDiscount="17"
+          MinusOrPlus="-"
+        />{" "}
+        <PishnahadCards
+          ImageAddress={img1}
+          SecondImageAddress={img2}
+          RingName="حلقه زیبا"
+          FirstPrice="500000"
+          SecondPrice="900000"
+          MySecond="57"
+          MyMiniute="35"
+          MyHour="11"
+          MyDays="143"
+          MyDiscount="17"
+          MinusOrPlus="-"
+        />{" "}
+        <PishnahadCards
+          ImageAddress={img1}
+          SecondImageAddress={img2}
+          RingName="حلقه زیبا"
+          FirstPrice="500000"
+          SecondPrice="900000"
+          MySecond="57"
+          MyMiniute="35"
+          MyHour="11"
+          MyDays="143"
+          MyDiscount="17"
+          MinusOrPlus="-"
+        />{" "}
+        <PishnahadCards
+          ImageAddress={img1}
+          SecondImageAddress={img2}
+          RingName="حلقه زیبا"
+          FirstPrice="500000"
+          SecondPrice="900000"
+          MySecond="57"
+          MyMiniute="35"
+          MyHour="11"
+          MyDays="143"
+          MyDiscount="17"
+          MinusOrPlus="-"
+        />
+        </div>
+      </div>
   );
 }
--- src/Pages/AnotherPage.jsx
+++ src/Pages/AnotherPage.jsx
@@ -30,82 +30,82 @@ function AnotherPage() {
       <hr />
       <WoodHeader />
       <hr />
-      <MyHeader/>
+      <MyHeader />
       <hr />
       <HamburgerBar />
       <br />
       <br />
-     <div className="flex flex-col md:flex-row justify-start gap-6">
-     <div className="flex flex-col justify-start w-[300px] ps-7">
-        <div className="filtereharaj shadow-lg p-5 rounded-lg">
-          <div
-            className=" bg-[#ececec] rounded-lg h-[50px] text-[16px] font-semibold
-            text-center pt-2 mb-1 shadow-md w-full
-            "
-          >
-            فیلتر موجودی و حراج
-          </div>
-          <div className="mt-3">
-            <CheckboxList />
-          </div>
-        </div>
-        <div className="filteregheimat shadow-lg p-5 rounded-lg">
-          <div>
+      <div className="flex flex-col md:flex-row justify-start gap-6">
+        <div className="flex flex-col justify-start w-[300px] ps-7">
+          <div className="filtereharaj shadow-lg p-5 rounded-lg">
             <div
               className=" bg-[#ececec] rounded-lg h-[50px] text-[16px] font-semibold
-            text-center pt-3 mb-3 shadow-md
+            text-center pt-2 mb-1 shadow-md w-full
             "
             >
-              فیلتر قیمت
+              فیلتر موجودی و حراج
             </div>
-            <div
-              dir="ltr"
-              className="flex justify-center items-center  bg-white"
-            >
-              <DualRangeSlider />
+            <div className="mt-3">
+              <CheckboxList />
             </div>
-            <div className="mt-5">
-              <a
-                className=" bg-[#f7f7f7] rounded-lg h-[50px] text-[14px] font-semibold
-            text-center pt-2 mb-1 shadow-md text-black hover:text-slate-600 mt-2 p-2 "
-                href=""
+          </div>
+          <div className="filteregheimat shadow-lg p-5 rounded-lg">
+            <div>
+              <div
+                className=" bg-[#ececec] rounded-lg h-[50px] text-[16px] font-semibold
+            text-center pt-3 mb-3 shadow-md
+            "
               >
-                فیلتر
-              </a>
+                فیلتر قیمت
+              </div>
+              <div
+                dir="ltr"
+                className="flex justify-center items-center  bg-white"
+              >
+                <DualRangeSlider />
+              </div>
+              <div className="mt-5">
+                <a
+                  className=" bg-[#f7f7f7] rounded-lg h-[50px] text-[14px] font-semibold
+            text-center pt-2 mb-1 shadow-md text-black hover:text-slate-600 mt-2 p-2 "
+                  href=""
+                >
+                  فیلتر
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="dastehaymahsoul shadow-lg p-5 rounded-lg">
+            <div
+              className="bg-[#ececec] rounded-lg h-[50px] text-[16px] font-semibold
+            text-center pt-3 mb-3 shadow-md"
+            >
+              دسته های محصولات
+            </div>
+            <div className="flex flex-col space-y-2 text-gray-400">
+              <a className="hover:text-black focus:text-black focus:font-bold" href="">دسته بندی نشده</a>
+              <a className="hover:text-black focus:text-black focus:font-bold" href="">پردازنده CPU</a>
+              <a className="hover:text-black focus:text-black focus:font-bold" href="">کارت گرافیک</a>
+              <a className="hover:text-black focus:text-black focus:font-bold" href="">مادربورد</a>
+              <a className="hover:text-black focus:text-black focus:font-bold" href="">رم دسکتاپ</a>
+              <a className="hover:text-black focus:text-black focus:font-bold" href="">هارد SSD</a>
+              <a className="hover:text-black focus:text-black focus:font-bold" href="">پاور</a>
+              <a className="hover:text-black focus:text-black focus:font-bold" href="">کیس</a>
             </div>
           </div>
         </div>
-        <div className="dastehaymahsoul shadow-lg p-5 rounded-lg">
-          <div
-            className="bg-[#ececec] rounded-lg h-[50px] text-[16px] font-semibold
-            text-center pt-3 mb-3 shadow-md"
-          >
-            دسته های محصولات
-          </div>
-          <div className="flex flex-col space-y-2 text-gray-400">
-            <a className="hover:text-black focus:text-black focus:font-bold" href="">دسته بندی نشده</a>
-            <a className="hover:text-black focus:text-black focus:font-bold" href="">پردازنده CPU</a>
-            <a className="hover:text-black focus:text-black focus:font-bold" href="">کارت گرافیک</a>
-            <a className="hover:text-black focus:text-black focus:font-bold" href="">مادربورد</a>
-            <a className="hover:text-black focus:text-black focus:font-bold" href="">رم دسکتاپ</a>
-            <a className="hover:text-black focus:text-black focus:font-bold" href="">هارد SSD</a>
-            <a className="hover:text-black focus:text-black focus:font-bold" href="">پاور</a>
-            <a className="hover:text-black focus:text-black focus:font-bold" href="">کیس</a>
-          </div>
+        <div className="secondPart">
+          <BreadcrumbSearch />
         </div>
       </div>
-      <div className="secondPart">
-<BreadcrumbSearch/>
-      </div>
-     </div>
       <br />
-    <div className="flex justify-center">
-    <HoverImageEffect/>
-    </div>
-    <br /><br />
-    <div >
-<Basefile/>
-    </div>
+      <div className="flex justify-center">
+        {/* <HoverImageEffect /> */}
+      </div>
+      <br /><br />
+      <div >
+        {/* <Basefile /> */}
+      </div>
       <br />
       <ScrollToTopButton />
       <br />
