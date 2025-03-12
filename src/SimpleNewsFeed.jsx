10045061ec0e7e9cb7da17e940488924a908f30b
AliMadadi <alimadadi110@gmail.com>
2025-03-12 09:18:35 +0330

    simple NewsFeed deactivated to clean the code and mobile view

--- src/SimpleNewsFeed.jsx
+++ src/SimpleNewsFeed.jsx
@@ -3,23 +3,8 @@ import "./index.css"
 
 function SimpleNewsFeed() {
   return (
-    <div>
-
-
-    <div className="news-container bg-gradient-to-l  from-[#c7a984] to-orange-100 block sticky z-50 sm:hidden">
-        
-
-        
-        <ul>
-           <li>متن اول</li>
-           <li>متن دوم</li>
-           <li>متن سوم</li>
-           <li>متن چهارم</li>
-        </ul>
-    </div>
-
-
-    </div>
+    <>
+    </>
   )
 }
 
