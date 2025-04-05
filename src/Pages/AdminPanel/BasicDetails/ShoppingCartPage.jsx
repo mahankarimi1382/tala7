751b1b7b98ac98e4f80eae421bd88895cd3bcdba
Ali <alimadadi110@gmail.com>
2025-04-05 15:29:40 +0330

    shopping Cart page completed

--- src/Pages/AdminPanel/BasicDetails/ShoppingCartPage.jsx
+++ src/Pages/AdminPanel/BasicDetails/ShoppingCartPage.jsx
@@ -6,13 +6,18 @@ import MyHeader from "../../../Components/MyHeader";
 import ScrollToTopButton from "../../../Components/ScrollToTopButton";
 import WrittenFooter from "../../../WrittenFooter";
 import MyFooter from "../../../MyFooter";
+import img1 from "../../../assets/img/containerring3.png";
+import img2 from "../../../assets/img/containerring4.png";
+import { GoTrash } from "react-icons/go";
+
+const convertToPersianDigits = (num) => {
+  return num.toLocaleString("fa-IR"); // Converts to Persian digits with thousand separators
+};
 
 function ShoppingCartPage() {
   return (
     <div>
       <NewsFeed />
-      <hr />
-      {/* <SimpleNewsFeed /> */}
       <hr />
       <WoodHeader />
       <hr />
@@ -20,28 +25,85 @@ function ShoppingCartPage() {
       <hr />
       <HamburgerBar />
       <hr />
+
       <div className="flex mx-11 mt-9 gap-4">
-        <div className="w-[70%]   p-2 ">
-      <div className="flex gap-2 items-baseline">
-      <p className="text-sm">سبد خرید شما</p>
-      <p className="text-xs">1 مرسوله</p>
-      </div>
+        {/* Left Section (Cart Summary) */}
+        <div className="w-[70%] p-4 border border-gray-200 rounded-lg">
+          {/* Title */}
+          <div className="flex gap-2 items-baseline mb-6">
+            <p className="text-lg font-medium">سبد خرید شما</p>
+            <p className="text-sm text-gray-600">1 مرسوله</p>
+          </div>
+
+          {/* Cart Item */}
+          <div className="flex gap-4 items-center p-4 border-b border-gray-200">
+            {/* Image Container */}
+            <div className="relative w-[120px] h-[120px]">
+            <div className="imageBox ">
+                <div className="imageInn ">
+                  <img
+                    className=" FirstImage rounded-t-lg block"
+                    src={img1}
+                    alt="Main Image"
+                  />
+                </div>
+
+                <div className=" SecondImage  hoverable-object rounded-t-lg block hoverImg  ">
+                  <img
+                    className=""
+                    src={img2}
+                    alt="Second Image"
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* Product Name */}
+            <p className="text-md font-medium">حلقه رزگلد زیبا</p>
+          </div>
+
+          {/* Price & Delete Button */}
+          <div className="flex justify-between items-center p-4">
+            <p className="text-gray-600 text-lg">
+              {convertToPersianDigits(2370000)} تومان
+            </p>
+            <button className="text-red-500 hover:text-red-700 p-2 rounded-full">
+              <GoTrash size={20} />
+            </button>
+          </div>
         </div>
-        <div className="w-[25%] border border-gray-200 rounded-lg p-2">
-          
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          voluptates, quisquam quia animi ea laboriosam suscipit consequatur
-          reiciendis illum cupiditate temporibus harum vel debitis quo dolor
-          ratione ipsa commodi error?
+
+        {/* Right Section (Price Details) */}
+        <div className="w-[25%] border border-gray-200 rounded-lg p-5 flex flex-col space-y-4">
+          <div className="flex justify-between text-[14px]">
+            <p className="text-gray-600">قیمت کالاها</p>
+            <p className="text-gray-600 text-right">
+              {convertToPersianDigits(2370000)} تومان
+            </p>
+          </div>
+          <div className="flex justify-between text-[14px]">
+            <p>جمع سبد خرید</p>
+            <p className="text-right">
+              {convertToPersianDigits(4370000)} تومان
+            </p>
+          </div>
+          <div className="flex justify-between text-[14px]">
+            <p className="text-green-600">سود شما از خرید</p>
+            <p className="text-green-600 text-right">
+              {convertToPersianDigits(2000000)} تومان
+            </p>
+          </div>
+
+          {/* Confirmation Button */}
+          <button className="bg-rose-500 hover:bg-rose-600 transition text-white text-center rounded-md py-2 w-full text-[15px] font-medium">
+            تأیید و تکمیل سفارش
+          </button>
         </div>
       </div>
+
       <ScrollToTopButton />
       <br />
-      <br />
-      <br />
-
       <WrittenFooter />
-
       <MyFooter />
     </div>
   );
