3f87b23f73f3e0588565b72b92d773ab7028845f
Ali <alimadadi110@gmail.com>
2025-04-05 15:48:12 +0330

    Shopping Cart now responsive

--- src/Pages/AdminPanel/BasicDetails/ShoppingCartPage.jsx
+++ src/Pages/AdminPanel/BasicDetails/ShoppingCartPage.jsx
@@ -26,9 +26,9 @@ function ShoppingCartPage() {
       <HamburgerBar />
       <hr />
 
-      <div className="flex mx-11 mt-9 gap-4">
+      <div className="flex flex-col lg:flex-row mx-5 lg:mx-11 mt-9 gap-4">
         {/* Left Section (Cart Summary) */}
-        <div className="w-[70%] p-4 border border-gray-200 rounded-lg">
+        <div className="w-full lg:w-[70%] p-4 rounded-lg">
           {/* Title */}
           <div className="flex gap-2 items-baseline mb-6">
             <p className="text-lg font-medium">سبد خرید شما</p>
@@ -36,30 +36,27 @@ function ShoppingCartPage() {
           </div>
 
           {/* Cart Item */}
-          <div className="flex gap-4 items-center p-4 border-b border-gray-200">
+          <div className="flex flex-col sm:flex-row gap-4 items-center p-4 border-b border-gray-200">
             {/* Image Container */}
             <div className="relative w-[120px] h-[120px]">
-            <div className="imageBox ">
-                <div className="imageInn ">
+              <div className="imageBox">
+                <div className="imageInn">
                   <img
-                    className=" FirstImage rounded-t-lg block"
+                    className="FirstImage rounded-t-lg block"
                     src={img1}
                     alt="Main Image"
                   />
                 </div>
-
-                <div className=" SecondImage  hoverable-object rounded-t-lg block hoverImg  ">
-                  <img
-                    className=""
-                    src={img2}
-                    alt="Second Image"
-                  />
+                <div className="SecondImage hoverable-object rounded-t-lg block hoverImg">
+                  <img className="" src={img2} alt="Second Image" />
                 </div>
               </div>
             </div>
 
             {/* Product Name */}
-            <p className="text-md font-medium">حلقه رزگلد زیبا</p>
+            <p className="text-md font-medium text-center sm:text-left">
+              حلقه رزگلد زیبا
+            </p>
           </div>
 
           {/* Price & Delete Button */}
@@ -74,7 +71,7 @@ function ShoppingCartPage() {
         </div>
 
         {/* Right Section (Price Details) */}
-        <div className="w-[25%] border border-gray-200 rounded-lg p-5 flex flex-col space-y-4">
+        <div className="w-full lg:w-[25%] h-auto border border-gray-200 rounded-lg p-5 flex flex-col space-y-4">
           <div className="flex justify-between text-[14px]">
             <p className="text-gray-600">قیمت کالاها</p>
             <p className="text-gray-600 text-right">
