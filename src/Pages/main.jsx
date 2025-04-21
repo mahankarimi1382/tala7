933351913e913659a4a5486d3e4e56fc2b15df6b
mahan karimi <13mahankarimi82@gmail.com>
2025-04-21 17:33:47 +0330

    fix erors

--- src/Pages/main.jsx
+++ src/Pages/main.jsx
@@ -26,7 +26,7 @@ import AllProducts from "../AllProducts.jsx";
 import ShoppingCartPage from "./AdminPanel/BasicDetails/ShoppingCartPage.jsx";
 import InstallmentRequest from "../InstallmentRequest.jsx";
 import InstallmentTrack from "../InstallmentTrack.jsx";
-import InstallmentInspection from "./AdminPanel/BasicDetails/InstallmentInspection.jsx";
+// import InstallmentInspection from "./AdminPanel/BasicDetails/InstallmentInspection.jsx";
 
 
 createRoot(document.getElementById("root")).render(
@@ -76,7 +76,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/AdminPannel/news" element={<News />} />
         <Route path="/AddSomeNews" element={<AddNews />} />
         <Route path="/AllProducts" element={<AllProducts />} />
-        <Route path="/AdminPannel/InstallmentInspection" element={<InstallmentInspection />} />
+        {/* <Route path="/AdminPannel/InstallmentInspection" element={<InstallmentInspection />} /> */}
       </Routes>
     </BrowserRouter>
     <ToastContainer />
